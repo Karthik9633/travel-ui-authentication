ebb68e29b845924f687d7c33f8ea19dd8817a1e0
Karthik B <karthikbalu5533@gmail.com>
2026-01-14 00:23:46 +0530

    created a function for signupForm and stored name email etc in different varibles

--- js/index.js
+++ js/index.js
@@ -22,10 +22,10 @@ if (signupForm) {
         e.preventDefault()
         let valid = true
 
-        const name = document.getElementById("name");
-        const email = document.getElementById("email");
-        const phone = document.getElementById("phone");
-        const pass = document.getElementById("password");
-        const confirm = document.getElementById("confirmPassword");
+        const name = document.getElementById("name")
+        const email = document.getElementById("email")
+        const phone = document.getElementById("phone")
+        const pass = document.getElementById("password")
+        const confirm = document.getElementById("confirmPassword")
     })
 }
