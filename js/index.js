4d50ac5dfc6191f41ce150bcd0bd57b681bd60c9
Karthik B <karthikbalu5533@gmail.com>
2026-01-14 00:22:35 +0530

    created function for add and remove errors

--- js/index.js
+++ js/index.js
@@ -14,3 +14,18 @@ function clearError(input) {
     error.innerHTML = "";
     input.classList.remove("is-invalid");
 }
+
+const signupForm = document.getElementById("signupForm")
+
+if (signupForm) {
+    signupForm.addEventListener('submit', e => {
+        e.preventDefault()
+        let valid = true
+
+        const name = document.getElementById("name");
+        const email = document.getElementById("email");
+        const phone = document.getElementById("phone");
+        const pass = document.getElementById("password");
+        const confirm = document.getElementById("confirmPassword");
+    })
+}
