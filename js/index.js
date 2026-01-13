225da808b9c9cf7fdce7492b7d13d20032103129
Karthik B <karthikbalu5533@gmail.com>
2026-01-14 00:17:33 +0530

    created function for add and remove errors

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@ function toggle(id) {
     const input = document.getElementById(id)
     input.type = input.type === "password" ? "text" : "password"
 }
+
+function showError(input, msg) {
+    const error = input.closest(".mb-3").querySelector(".error");
+    error.innerHTML = msg;
+    input.classList.add("is-invalid");
+}
+
+function clearError(input) {
+    const error = input.closest(".mb-3").querySelector(".error");
+    error.innerHTML = "";
+    input.classList.remove("is-invalid");
+}
